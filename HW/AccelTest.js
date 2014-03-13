39f0aafc459423a25e323ad94ed2fd6f0dbd6f39
jcb871 <jcb871@gmail.com>
2014-03-13 13:46:17 -0500

    Create AccelTest.js

---
+++ HW/AccelTest.js
@@ -0,0 +1,35 @@
+console.log('Simply.js demo!');
+
+simply.on('singleClick', function(e) {
+  console.log(util2.format('single clicked $button!', e));
+  if(e.button == 'select')
+  { 
+    simply.on('accelData', function(e) {
+  simply.subtitle('Loading...');
+  console.log(util2.format('Got $samples samples.', e));
+  var accData = '';
+  for(var k=0;k<e.samples;k++)
+  {
+      var sample = e.accels[k];
+      accData += String.format('x: {0}, y:{1}, z:{2}\n', sample.x, sample.y, sample.z);
+  }
+  simply.setText({subtitle:'updated', body: accData});
+  simply.off('accelData');
+   });
+  }  
+});
+
+if (!String.format) {
+  String.format = function(format) {
+    var args = Array.prototype.slice.call(arguments, 1);
+    return format.replace(/{(\d+)}/g, function(match, number) { 
+      return typeof args[number] != 'undefined'? args[number] : match;
+    });
+  };
+}
+
+simply.setText({
+  title: 'Accel Demo!',
+  body: 'Press buttons or tap the watch!',
+}, true);
+
