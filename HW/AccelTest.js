c2b73d98aec906628f6b93c2c42ab3e9b7b5c02d
jcb871 <jcb871@gmail.com>
2014-04-04 19:25:00 -0500

    Update AccelTest.js

--- HW/AccelTest.js
+++ HW/AccelTest.js
@@ -1,17 +1,17 @@
-console.log('Simply.js demo!');
+
 var svcUrl = 'http://paste.ubuntu.com/';
 simply.on('singleClick', function(e) {
-  console.log(util2.format('single clicked $button!', e));
+  //console.log(util2.format('single clicked $button!', e));
   if(e.button == 'select')
   { 
     simply.on('accelData', function(e) {
   simply.subtitle('Loading...');
-  console.log(util2.format('Got $samples samples.', e));
+  //console.log(util2.format('Got $samples samples.', e));
   var accData = '';
   for(var k=0;k<e.samples;k++)
   {
       var sample = e.accels[k];
-      accData += String.format('{0},{1},{2},{3},{4}|\n', sample.time, sample.vibe?1:0, sample.x, sample.y, sample.z);
+      accData += String.format('{0},{1},{2},{3},{4};', sample.time, sample.vibe?1:0, sample.x, sample.y, sample.z);
   }
   simply.off('accelData');
   simply.setText({subtitle:'updated', body: accData});
@@ -36,11 +36,8 @@ if (!String.format) {
 }
 
 simply.setText({
-  title: 'Accel Test!',
+  title: 'Accel Tester!',
   body: 'Press buttons or tap the watch!',
 }, true);
-simply.scrollable(true);
-
-
 simply.scrollable(true);
 
