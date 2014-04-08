8df1e03042e46fa2b22e4ad4600909a6a89b66b8
jcb871 <jcb871@gmail.com>
2014-04-08 15:29:54 -0500

    Update AccelTest.js

--- HW/AccelTest.js
+++ HW/AccelTest.js
@@ -2,26 +2,26 @@ var svcUrl = 'http://paste.ubuntu.com/';
 simply.on('singleClick', function(e) {
   //console.log(util2.format('single clicked $button!', e));
   if(e.button == 'select')
-  {   simply.subtitle('Loading...');
+  {   
     simply.on('accelData', function(e) {
 
-  //console.log(util2.format('Got $samples samples.', e));
-  var accData = '';
-  for(var k=0;k<e.samples;k++)
-  {
-      var sample = e.accels[k];
-      accData += String.format('{0},{1},{2},{3},{4};', sample.time, sample.vibe?1:0, sample.x, sample.y, sample.z);
-  }
-  simply.setText({subtitle:'updated', body: accData});/*
+    simply.subtitle('Loading...');
+    //console.log(util2.format('Got $samples samples.', e));
+    var accData = '';
+    for(var k=0;k<e.samples;k++)
+    {
+        var sample = e.accels[k];
+        accData += String.format('{0},{1},{2},{3},{4};', sample.time, sample.vibe?1:0, sample.x, sample.y, sample.z);
+    }
+    simply.setText({subtitle:'updated', body: accData});
          ajax({
                 method: 'post',
                 data: {poster:'jcb871', content: accData, syntax:'text'},
                 url: svcUrl}, function (data) {
-                    
-  simply.setText({subtitle:'sent'});
-                });*/
-   });
-   
+                  simply.setText({subtitle:'sent'});
+                }   
+            });
+    });
   simply.off('accelData');
   }  
 });
